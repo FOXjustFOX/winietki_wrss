06051a5dda4cbfd133334a8e7893de14c5ad7253
Igor Lis <87518349+FOXjustFOX@users.noreply.github.com>
2025-09-20 00:44:22 +0200

    changed the node base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    base: '/winietki/',
     plugins: [react()],
     optimizeDeps: {
         include: ["pdfjs-dist/build/pdf.worker.min.mjs"],
