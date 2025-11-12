378b8c9384e3d80a780ae6da13480f596ca0f1cc
Igor Lis <87518349+FOXjustFOX@users.noreply.github.com>
2025-11-12 18:49:06 +0100

    Remove base path configuration from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    base: "/',
     plugins: [react()],
     optimizeDeps: {
         include: ["pdfjs-dist/build/pdf.worker.min.mjs"],
